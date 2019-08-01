f87679a489eaa17bdde04b8a22485cf19607d825
thilak ramu <thilakramu99@gmail.com>
2019-08-01 08:36:50 +0530

    intial code for file upload

--- src/main/java/com/example/chat/service/UserPhotoService.java
+++ src/main/java/com/example/chat/service/UserPhotoService.java
@@ -24,6 +24,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
+import java.util.UUID;
 
 import com.example.chat.service.UserService;
 
@@ -45,22 +46,34 @@ public class UserPhotoService {
         }
 
         try {
+        	
+        	String fileName = file.getOriginalFilename();     	
+        	
+        	int i = fileName.lastIndexOf('.');
+        	String fileExt = fileName.substring(i);
+        	
+        	UUID uuid = UUID.randomUUID();
+    		String randomUUIDString = uuid.toString();
+    		String filePath = UPLOADED_FOLDER + randomUUIDString + fileExt;
+    		String newFileName = randomUUIDString + fileExt;
 
             // Get the file and save it somewhere
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Path path = Paths.get(filePath);
             Files.write(path, bytes);
             
             Integer userId = (Integer) request.getSession().getAttribute("user_id");
     		
     		User user = userService.getUserById(userId);
-            
+    		            
             UserPhoto up = new UserPhoto();
             up.setUser(user);
-            up.setFilePath(UPLOADED_FOLDER + file.getOriginalFilename());
+            up.setUuid(randomUUIDString);
+            up.setFileExt(fileExt);
+            up.setFilePath(filePath);
             up.setFileSize(file.getSize());
             up.setFileType(file.getContentType());
-            up.setFileName(file.getOriginalFilename());
+            up.setFileName(newFileName);
             up.setCreatedOn(new Date());
             up.setUpdatedOn(new Date());
             
